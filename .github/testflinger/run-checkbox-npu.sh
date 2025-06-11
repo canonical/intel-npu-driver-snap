995e5cc1b8a62276e3fca5dfafc1effab5035591
Will French <will.french@canonical.com>
2025-06-11 11:10:57 -0500

    Reset device node permissions following a reboot

---
+++ .github/testflinger/run-checkbox-npu.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -e
+
+usermod -a -G render ubuntu
+# Newer versions of Ubuntu ship Udev rules which handle this,
+# but 22.04 does not
+chown root:render /dev/accel/accel0
+chmod g+rw /dev/accel/accel0
+
+checkbox-npu.install-full-deps
+
+echo
+echo "========= Checkbox NPU ========="
+echo "====== DEVICE KERNEL INFO ======"
+echo
+uname -a
+echo
+echo "================================"
+echo
+
+checkbox-npu.test-runner-automated
