9362c0f2974a4d2f039a86b81d78c1dae79e1660
Will French <will.french@canonical.com>
2025-06-11 11:10:57 -0500

    Reset device node permissions following a reboot

--- .github/testflinger/check-npu-fw.sh
+++ .github/testflinger/check-npu-fw.sh
@@ -1,6 +1,5 @@
 #!/bin/bash -e
 
-set -e
 fw_search_path=$(sudo cat /sys/module/firmware_class/parameters/path)
 echo "[INFO]: fw_search_path: ${fw_search_path}"
 echo "[INFO]: $(snap services intel-npu-driver.load-npu-firmware)"
