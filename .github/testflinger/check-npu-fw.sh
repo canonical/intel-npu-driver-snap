a5a11c08693aae9f2651404fbb0b79a7dbd60910
Will French <will.french@canonical.com>
2025-06-16 15:30:09 -0500

    Refactor integration testing (#19)
    * Refactor CI to follow cert team best practices
    
    * Use correct runners, checkout source before tagging
    
    * Switch back to self-hosted runner as private endpoint not working
    
    * Build checkbox provider snap once
    
    * Add missing key to YAML
    
    * Put attatchments on device
    
    * Improve resiliency, switch back to private endpoint runners
    
    * Fix TF queue, disable fail-fast
    
    * Reset device node permissions following a reboot
    
    * Prefer apt upgrade as it does not hold back kernel upgrades
    
    * Remove beta flag now that snap is published in stable channels
    
    * Remove testing branch

---
+++ .github/testflinger/check-npu-fw.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -e
+
+fw_search_path=$(sudo cat /sys/module/firmware_class/parameters/path)
+echo "[INFO]: fw_search_path: ${fw_search_path}"
+echo "[INFO]: $(snap services intel-npu-driver.load-npu-firmware)"
+echo "[INFO]: $(sudo snap logs intel-npu-driver.load-npu-firmware)"
+if [ -z "${fw_search_path}" ]; then
+  >&2 echo "Test error: Firmware search path not updated."
+  exit 1
+fi
+echo "Checking for firmware in the expected path ${fw_search_path}/intel/vpu"
+ls "${fw_search_path}"/intel/vpu/ | grep ".*.bin$"
+echo "Test success: Found .bin file(s) in the expected path."
