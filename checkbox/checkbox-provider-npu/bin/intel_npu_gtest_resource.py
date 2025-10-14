0caa317c839bbde3ad96d9440be242ec444bb773
Vanda Hendrychová <vanda.hendrychova@canonical.com>
2025-10-14 15:07:31 +0200

    remove known failures, use metric_streamer and ivpu_bo_create resources

--- checkbox/checkbox-provider-npu/bin/intel_npu_gtest_resource.py
+++ checkbox/checkbox-provider-npu/bin/intel_npu_gtest_resource.py
@@ -21,14 +21,39 @@ def main() -> int:
             category, test_name = line.split('.', 1)
     
             extra_flags = []
+            metric_streamer_allowed_states = [ "supported", "unsupported" ]
+            ivpu_bo_create_allowed_states = [ "supported", "unsupported" ]
+            known_failure = False
+
             if category.startswith("ZeInit"):
                 extra_flags.append("--ze-init-tests")
+
+            if category.startswith("Metric"):
+                metric_streamer_allowed_states = [ "supported" ]
     
-            print_as_resource({
-                "name": test_name,
-                "category": category,
-                "extra_flags": " ".join(extra_flags)
-                })
+            if category in ["CompilerInDriverWithProfiling", "CommandMemoryFill" ] or\
+                    line == "ImmediateCmdList.FillCopyUsingBarriers":
+                ivpu_bo_create_allowed_states = [ "supported" ]
+
+            # Device.GetZesEngineGetActivity -- No sysfs access for NPU activity data
+            # ExternalMemoryZe.GpuZeFillToNpuZeCopy -- No GPU support for test
+            # ExternalMemoryZe.NpuZeFillToGpuZeCopy -- No GPU support for test
+            # ExternalMemoryDmaHeap.DmaHeapToNpu/2KB -- requires access to /dev/dma_heap/system
+            # ExternalMemoryDmaHeap.DmaHeapToNpu/16MB -- requires access to /dev/dma_heap/system
+            # ExternalMemoryDmaHeap.DmaHeapToNpu/255MB -- requires access to /dev/dma_heap/system
+            if test_name.find("Gpu") != -1 or \
+                    category.find("DmaHeap") != -1 or \
+                    line == "Device.GetZesEngineGetActivity":
+                known_failure = True
+
+            if not known_failure:
+                print_as_resource({
+                    "name": test_name,
+                    "category": category,
+                    "extra_flags": " ".join(extra_flags),
+                    "metric_streamer_allowed_states": metric_streamer_allowed_states,
+                    "ivpu_bo_create_allowed_states": ivpu_bo_create_allowed_states,
+                    })
 
 if __name__ == "__main__":
     sys.exit(main())
