7ee8891bd1f4a394332477ebe98cc44a125386a8
Vanda Hendrychová <vanda.hendrychova@canonical.com>
2025-10-24 13:22:25 +0200

    move extra flags, metric streamer support etc. into their own functions

--- checkbox/checkbox-provider-npu/bin/intel_npu_gtest_resource.py
+++ checkbox/checkbox-provider-npu/bin/intel_npu_gtest_resource.py
@@ -4,15 +4,55 @@ import subprocess
 import sys
 
 
-def print_resource_line(key: str, value: str) -> None:
-    print(f"{key}: {value}")
-
-
 def print_as_resource(d: dict) -> None:
     for k, v in d.items():
-        print_resource_line(k, v)
+        print(f"{k}: {v}")
 
     print("")
+
+
+def get_extra_flags() -> list[str]:
+    extra_flags = []
+    if category.startswith("ZeInit"):
+        extra_flags.append("--ze-init-tests")
+    return extra_flags
+
+
+def get_metric_streamer_allowed_states(category: str) -> list[str]:
+    if category.startswith("Metric"):
+        return ["supported"]
+    else:
+        return ["supported", "unsupported"]
+
+
+def get_ivpu_bo_create_allowed_states(category: str, test_name: str) -> list[str]:
+    if category in ["CompilerInDriverWithProfiling",
+                    "CommandMemoryFill"] or\
+            (category  == "ImmediateCmdList" and test_name == "FillCopyUsingBarriers"):
+        ivpu_bo_create_allowed_states = ["supported"]
+    else:
+        return ["supported", "unsupported"]
+
+# Known failures:
+# - Device.GetZesEngineGetActivity:
+#     No sysfs access for NPU activity data
+# - ExternalMemoryZe.GpuZeFillToNpuZeCopy:
+#     No GPU support for test
+# - ExternalMemoryZe.NpuZeFillToGpuZeCopy:
+#     No GPU support for test
+# - ExternalMemoryDmaHeap.DmaHeapToNpu/2KB:
+#     requires access to /dev/dma_heap/system
+# - ExternalMemoryDmaHeap.DmaHeapToNpu/16MB:
+#     requires access to /dev/dma_heap/system
+# - ExternalMemoryDmaHeap.DmaHeapToNpu/255MB:
+#     requires access to /dev/dma_heap/system
+def is_known_failure(category, test_name):
+    if test_name.find("Gpu") != -1 or \
+            category.find("DmaHeap") != -1 or \
+            (category == "Device" and test_name == "GetZesEngineGetActivity"):
+        return True
+    else:
+        return False
 
 
 def main() -> int:
@@ -27,49 +67,20 @@ def main() -> int:
             category, test_name = line.split('.', 1)
 
             extra_flags = []
-            metric_streamer_allowed_states = ["supported", "unsupported"]
-            ivpu_bo_create_allowed_states = ["supported", "unsupported"]
-            known_failure = False
+            metric_streamer_allowed_states = get_metric_streamer_allowed_states(category)
+            ivpu_bo_create_allowed_states = get_ivpu_bo_create_allowed_states(category, test_name)
+            known_failure = is_known_failure(category, test_name)
 
-            if category.startswith("ZeInit"):
-                extra_flags.append("--ze-init-tests")
-
-            if category.startswith("Metric"):
-                metric_streamer_allowed_states = ["supported"]
-
-            if category in ["CompilerInDriverWithProfiling",
-                            "CommandMemoryFill"] or\
-                    line == "ImmediateCmdList.FillCopyUsingBarriers":
-                ivpu_bo_create_allowed_states = ["supported"]
-
-            # Device.GetZesEngineGetActivity:
-            #   No sysfs access for NPU activity data
-            # ExternalMemoryZe.GpuZeFillToNpuZeCopy:
-            #   No GPU support for test
-            # ExternalMemoryZe.NpuZeFillToGpuZeCopy:
-            #   No GPU support for test
-            # ExternalMemoryDmaHeap.DmaHeapToNpu/2KB:
-            #   requires access to /dev/dma_heap/system
-            # ExternalMemoryDmaHeap.DmaHeapToNpu/16MB:
-            #   requires access to /dev/dma_heap/system
-            # ExternalMemoryDmaHeap.DmaHeapToNpu/255MB:
-            #   requires access to /dev/dma_heap/system
-            if test_name.find("Gpu") != -1 or \
-                    category.find("DmaHeap") != -1 or \
-                    line == "Device.GetZesEngineGetActivity":
-                known_failure = True
-
-            if not known_failure:
-                print_as_resource({
-                    "name": test_name,
-                    "category": category,
-                    "extra_flags": " ".join(extra_flags),
-                    "metric_streamer_allowed_states":
-                        metric_streamer_allowed_states,
-                    "ivpu_bo_create_allowed_states":
-                        ivpu_bo_create_allowed_states,
-                    })
-    return 0
+            print_as_resource({
+                "name": test_name,
+                "category": category,
+                "extra_flags": " ".join(extra_flags),
+                "metric_streamer_allowed_states":
+                    metric_streamer_allowed_states,
+                "ivpu_bo_create_allowed_states":
+                    ivpu_bo_create_allowed_states,
+                "known_failure": known_failure
+                })
 
 
 if __name__ == "__main__":
